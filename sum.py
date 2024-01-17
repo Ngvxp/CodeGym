820ba9b4bcbd19b462056cbfd94a8532fd4e0434
Ngvxp <ngovuxuanphuong@gmail.com>
2024-01-17 23:26:30 +0900

    lesson 3 - bien, toan tu, kieu dl

---
+++ sum.py
@@ -0,0 +1,9 @@
+# INPUT A,B
+A = 5
+B = 7
+
+# XU LI
+C = A + B
+
+#OUTPUT
+print(C)
---
+++ variable.py
@@ -0,0 +1,5 @@
+#luyen tap khai bao bien
+groupName = " this's is python Basic"
+number = 20
+single = True
+size = 'xxkg'
